c1f9d3dc7685a96a37b10326f84c5ae6b5ff5247
Frank Mathy <frankmathy@gmail.com>
2017-12-20 21:32:52 +0100

    Cleaned class

--- Expenses/Category.swift
+++ Expenses/Category.swift
@@ -6,34 +6,13 @@
 //  Copyright © 2017 Frank Mathy. All rights reserved.
 //
 
-import Foundation
-import os.log
-
-class Category: NSObject, NSCoding {
+class Category {
     var name: String?
-    
-    //MARK: Types
-    struct PropertyKey {
-        static let name = "name"
-    }
-
     
     init?(name: String) {
         guard !name.isEmpty else {
             return nil
         }
         self.name = name
-    }
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(name, forKey: PropertyKey.name)
-    }
-    
-    required convenience init?(coder aDecoder: NSCoder) {
-        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
-            os_log("Unable to decode the name for a Category object.", log: OSLog.default, type:.error)
-            return nil
-        }
-        self.init(name: name)
     }
 }
