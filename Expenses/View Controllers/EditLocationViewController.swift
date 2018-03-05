a51d82989cc8cc7d967bc25a9a9bea358964a987
Frank Mathy <frankmathy@gmail.com>
2018-03-06 00:15:24 +0100

    Enhanced map

---
+++ Expenses/View Controllers/EditLocationViewController.swift
@@ -0,0 +1,111 @@
+//
+//  EditLocationViewController.swift
+//  Expenses
+//
+//  Created by Frank Mathy on 05.03.18.
+//  Copyright © 2018 Frank Mathy. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class EditLocationViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var placesTableView: UITableView!
+
+    let locationManager = CLLocationManager()
+    var currentLocation : CLLocation?
+
+    let regionRadius: CLLocationDistance = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        mapView.delegate = self
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            switch(CLLocationManager.authorizationStatus()) {
+            case .authorizedWhenInUse:
+                locationManager.startUpdatingLocation()
+            default:
+                showLocationAlert()
+            }
+            locationManager.startUpdatingLocation()
+        } else {
+            showLocationAlert()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        defer { currentLocation = locations.last }
+        if currentLocation == nil {
+            // Zoom to user location
+            if let userLocation = locations.last {
+                self.locationManager.stopUpdatingLocation()
+                
+                let viewRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 2000, 2000)
+                mapView.setRegion(viewRegion, animated: false)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = userLocation.coordinate
+                annotation.title = "Location"
+                mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        switch(newState) {
+        case .starting:
+            view.dragState = .dragging
+        case .ending, .canceling:
+            view.dragState = .none
+        default: break
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKPointAnnotation {
+            let pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "VenuePin")
+            pinAnnotationView.pinTintColor = .red
+            pinAnnotationView.isDraggable = true
+            pinAnnotationView.canShowCallout = false
+            pinAnnotationView.animatesDrop = true
+            return pinAnnotationView
+        }
+        return nil
+    }
+    
+    
+    func showLocationAlert() {
+        let alert = UIAlertController(title: "Location Disabled", message: "Please enable location", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
